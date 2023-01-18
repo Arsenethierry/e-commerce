5cfeb626b905f4c3cfd32a2c761c33760db26a90
Arsene <komezarsene15@gmail.com>
2023-01-19 00:47:37 +0530

    login backend

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const products = require('./products');
 const register = require('./routes/register');
+const login = require('./routes/login');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/register", register);
+app.use("/api/login", login);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
